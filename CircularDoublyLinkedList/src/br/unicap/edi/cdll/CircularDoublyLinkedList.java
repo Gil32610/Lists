52c537bd4dd3ae5e7092b3d7334395b77dc87cf5
GIL32610 <gil.00000032610@unicap.br>
2022-11-04 10:29:18 -0300

    remove first

--- CircularDoublyLinkedList/src/br/unicap/edi/cdll/CircularDoublyLinkedList.java
+++ CircularDoublyLinkedList/src/br/unicap/edi/cdll/CircularDoublyLinkedList.java
@@ -61,20 +61,35 @@ public class CircularDoublyLinkedList<T extends Comparable<T>> {
     }
 
     // public void showAll() {
-    //     DoublyLinkedNode<T> current;
-    //     if (this.isEmpty()) {
-    //         System.out.println("Empty list");
-    //     } else {
-    //         while (true) {
-    //             if (current == this.head) {
-    //                 break;
-    //             }
-    //             current = this.head;
-    //             System.out.println(current.getContent());
-    //             current = current.getNext();
-
-    //         }
-    //     }
+    // DoublyLinkedNode<T> current;
+    // if (this.isEmpty()) {
+    // System.out.println("Empty list");
+    // } else {
+    // while (true) {
+    // if (current == this.head) {
+    // break;
+    // }
+    // current = this.head;
+    // System.out.println(current.getContent());
+    // current = current.getNext();
 
     // }
+    // }
+
+    // }
+
+    public void removeFirst() {
+        if (this.isEmpty()) {
+            System.out.println("Empty list");
+        } else if (this.head.getNext() == this.head) {
+            this.head = null;
+            this.tail = null;
+            this.quantity = 0;
+        } else {
+            this.head = this.head.getNext();
+            this.head.setPrevious(this.tail);
+            this.tail.setNext(this.head);
+            this.quantity--;
+        }
+    }
 }
