49b81fc80230f298ac6ff3cc181e98f97fb03fd2
GIL <gil.00000032610@unicap.br>
2022-11-07 18:47:05 -0300

    main method completed

--- AdvogadosSA/src/Prédio/Prédio.java
+++ AdvogadosSA/src/Prédio/Prédio.java
@@ -1,6 +1,9 @@
 package Prédio;
 
 import java.util.Scanner;
+
+import javax.swing.plaf.synth.SynthSplitPaneUI;
+
 import Pessoa.Pessoa;
 import Acesso.*;
 
@@ -9,6 +12,53 @@ public class Prédio {
         int op;
         String nome, RG;
         AdvogadosSA adv = new AdvogadosSA();
+        Scanner s = new Scanner(System.in);
+        Pessoa p;
+
+        do {
+            System.out.println("Selecione a operação: ");
+            options();
+            op = Integer.parseInt(s.nextLine());
+            switch (op) {
+                case 1:
+                    System.out.println("Informe o nome: ");
+                    nome = s.nextLine();
+                    System.out.println("Informe o RG:");
+                    RG = s.nextLine();
+                    p = new Pessoa(nome, RG);
+                    adv.cadastrar(p);
+                    System.out.println("Deseja cancelar a operação?");
+                    System.out.println("1- sim\n" + "2 - não");
+                    op = Integer.parseInt(s.nextLine());
+                    if (op == 1) {
+                        adv.cancelar();
+                    }
+
+                    break;
+                case 2:
+                    adv.exibir();
+                    break;
+                case 3:
+                    System.out.println("Informe o RG da pessoa: ");
+                    RG = s.nextLine();
+                    adv.verificar(RG);
+                    break;
+                case 4:
+                    adv.quantidade();
+                    break;
+                case 5:
+                    System.out.println("Informe o RG da pessoa: ");
+                    RG = s.nextLine();
+                    adv.remover(RG);
+                    break;
+                case 0:
+                    System.out.println("Concluído!");
+                    break;
+                default:
+                    System.out.println("Opção inválida!");
+                    break;
+            }
+        } while (op != 0);
 
     }
 
