545a7d8e46ad31ced62d3d7b3dbd052c9412221d
GIL <gil.00000032610@unicap.br>
2022-11-07 23:02:50 -0300

    main method

--- Decrescente/src/Integer/IntegerList.java
+++ Decrescente/src/Integer/IntegerList.java
@@ -7,10 +7,42 @@ public class IntegerList {
     public static void main(String[] args) {
         DoublyLinkedList<Integer> integerLinked = new DoublyLinkedList<Integer>(20);
         Scanner s = new Scanner(System.in);
+        int number;
         int op;
-        do{
+        do {
+            System.out.println("Selecione a opção: ");
+            options();
+            op = Integer.parseInt(s.nextLine());
+            switch (op) {
+                case 1:
+                System.out.println("Informe o inteiro: ");
+                number = Integer.parseInt(s.nextLine());
+                integerLinked.insertionSort(number);
+                    break;
+                case 2:
+                System.out.println("Informe o inteiro: ");
+                number = Integer.parseInt(s.nextLine());
+                integerLinked.removeNode(number);
+                    break;
+                case 3:
+                integerLinked.showAll();
+                    break;
+                case 4:
+                integerLinked.reverseShowAll();
+                    break;
+                case 5:
+                System.out.println("Informe o inteiro: ");
+                number = Integer.parseInt(s.nextLine());
+                integerLinked.searchContent(number);
+                    break;
+                case 0:
+                    break;
+                default:
+                System.out.println("Opção inválida!");
+                    break;
 
-        }while(op!= 0)
+            }
+        } while (op != 0);
 
     }
 
