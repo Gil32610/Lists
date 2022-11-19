9b33bb94aae945f897d0045a4faf0c68f098e11e
GIL <gil.00000032610@unicap.br>
2022-11-18 23:05:47 -0300

    aplication completed

--- AtividadeLDEC/src/Main/Main.java
+++ AtividadeLDEC/src/Main/Main.java
@@ -36,6 +36,9 @@ public class Main {
                     t = t4;
                     operateClass(t);
                     break;
+                case 0:
+                System.out.println("Até mais!");
+                    break;
                 default:
                     break;
             }
